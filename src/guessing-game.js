34cea32689fd5c0a5a871f6ec81b23d758613682
Oksana Fainitskaia <of@devinform.ru>
2019-09-24 17:43:34 +0300

    guessing-game/feature: solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
+        this.guessNumber = Math.round((this.max + this.min) / 2);
 
+        return this.guessNumber;
     }
 
     lower() {
-
+        this.max = this.guessNumber;
     }
 
     greater() {
-
+        this.min = this.guessNumber;
     }
 }
 
